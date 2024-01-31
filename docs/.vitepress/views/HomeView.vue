18424ffe97c5272bf9b5f8df8b6d594cd5fda8d7
柠檬冲水 <916415899@qq.com>
2024-01-31 16:44:07 +0800

    增加滚动

--- docs/.vitepress/views/HomeView.vue
+++ docs/.vitepress/views/HomeView.vue
@@ -1,41 +1,62 @@
 <script setup>
 import "./home.css";
-const Sponsorship = [
-    {
-        img: 'https://q1.qlogo.cn/g?b=qq&s=0&nk=1522099983',
-        title: '白狐脚本(Gitee)',
-        url: 'https://gitee.com/baihu433/Yunzai-Bot-Shell'
-    }, {
-        img: 'http://p.qlogo.cn/gh/946454868/946454868/100/',
-        title: '龙龙空间(QQ群)',
-        url: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=bgBJeCy1I2_DmJVQMv8ESmVHVO4opxXV&authKey=xFXMyhSDdd58KsxbQWvQaszjhia5cwTCRxFaNX1SmCvcj91XbWdYj15OghpzBJQk&noverify=0&group_code=946454868'
-    }, 
-    {
-        img: 'https://q1.qlogo.cn/g?b=qq&s=0&nk=3219073854',
-        title: '落夏()',
-        url: '/'
-    },
-    {
-        img: 'https://q1.qlogo.cn/g?b=qq&s=0&nk=3193524385',
-        title: '悠风起(Gitee)',
-        url: 'https://gitee.com/youwindy'
-    },
-    {
-        img: 'https://q1.qlogo.cn/g?b=qq&s=0&nk=2860450996',
-        title: '乐乐乐鱼()',
-        url: '/'
-    },
-    {
-        img: 'https://q1.qlogo.cn/g?b=qq&s=0&nk=2841644407',
-        title: '喵~()',
-        url: '/'
-    },
-    {
-        img: 'https://q1.qlogo.cn/g?b=qq&s=0&nk=2733977932',
-        title: '萧()',
-        url: '/'
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// data
+import { Sponsorship } from "./data";
+
+
+// import required modules
+import { Autoplay, Pagination } from 'swiper/modules';
+
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+const size = ref(1);
+
+// 监听变化
+const update = () => {
+    size.value = window.innerWidth < 600 ? 1 : 2;
+    if (window.innerWidth >= 860) {
+        size.value = 3;
     }
-]
+    if (window.innerWidth >= 1200) {
+        size.value = 4;
+    }
+    if (window.innerWidth >= 1400) {
+        size.value = 5;
+    }
+}
+
+onMounted(() => {
+    update()
+    // 监听视图宽度变化
+    window.addEventListener('resize', update);
+})
+
+// 组件注销后，清除监听
+onBeforeUnmount(() => {
+    window.removeEventListener('resize', update);
+})
+
+/**
+ * 自动滚动 Autoplay
+ * 间隙 spaceBetween
+ * 点击 Pagination
+ * 左右点击 navigation
+ * 循环滚动 loop
+ */
+
+const modules = [Autoplay, Pagination]
+
 </script>
 
 <template>
@@ -49,13 +70,28 @@ const Sponsorship = [
         <div class="list-title">
             Personal Sponsorship（个人赞助）
         </div>
+
         <div class="flex-wrap-center">
-            <a class="item" v-for="item of Sponsorship" :key="item.title" target="_blank" :href="item.url">
-                <img class="img" alt="" :src="item.img">
-                <div class="item-title">
-                    {{ item.title }}
-                </div>
-            </a>
+            <swiper :modules="modules" :spaceBetween="30" :loop="true" :slides-per-view="size" :autoplay="{
+                delay: 2500,
+                disableOnInteraction: false,
+            }" :pagination="{
+    clickable: true,
+    type: 'progressbar',
+}">
+                <swiper-slide class="item" v-for="item of Sponsorship" :key="item.name">
+                    <a target="_blank" :href="item.url">
+                        <img class="img" alt="" :src="item.img">
+                        <div class="item-title">
+                            {{ item.name }}
+                        </div>
+                        <div class="item-title">
+                            {{ item.doc }}
+                        </div>
+                    </a>
+                </swiper-slide>
+            </swiper>
+
         </div>
     </div>
 </template>
@@ -69,32 +105,34 @@ const Sponsorship = [
     max-width: 1200px;
 }
 
-.img {
-    border-radius: 20px;
-    width: 50px;
-    height: 50px;
-    margin: auto;
-}
-
-.item {
-    margin: 10px auto;
-    background-color: var(--vp-c-bg-soft);
-    padding: 10px 20px;
-    border-radius: 8px;
-}
-
-.item-title {
-    font-size: 18px;
-    margin-top: 10px;
-}
-
-
 .flex-wrap-center {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     padding: 10px 20px;
     max-width: 1200px;
+    margin: auto;
+    background-color: var(--vp-c-bg-soft);
+    border-radius: 8px;
+}
+
+.item {
+    margin-top: 20px;
+    padding: 10px 20px;
+    border-radius: 8px;
+    background-color: var(--vp-c-bg);
+}
+
+.item-title {
+    font-size: 18px;
+    margin-top: 10px;
+    text-align: center;
+}
+
+.img {
+    border-radius: 20px;
+    width: 50px;
+    height: 50px;
     margin: auto;
 }
 </style>
